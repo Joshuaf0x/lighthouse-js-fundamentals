c1a06292056934aa68131f9b59c8d5be7493b2c9
Josh Klein <josh.klein@live.ca>
2018-10-10 19:39:41 +0000

    cant get rid of the '' in the while loop

---
+++ ingredients.js
@@ -0,0 +1,16 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+var items = 0;
+while (items < 1) {
+  console.log(ingredients);
+  items++;
+}
+
+for (var i = 0; i <ingredients.length; i++) {
+  console.log(ingredients[i]);
+}
+
+for (var counter=ingredients.length; counter > 0; counter--) {
+  console.log(ingredients[counter]);
+}
+
